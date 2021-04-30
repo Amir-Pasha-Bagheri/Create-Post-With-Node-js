1c94f6e50cf14e080d2fc18289144084f39529a2
Amir Pasha Bagheri <a.p.bagheri83@gmail.com>
2021-04-30 18:14:43 +0430

    Changing Styles

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import { Router } from 'react-router';
+import Routes from './Pages/Routes'
+import history from './Pages/history'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router history={history}>
+        <Routes/>
+    </Router>
   );
 }
 
