fbab067634eb4cba7b95b066d22617cc0a68b4b3
Amir Pasha Bagheri <a.p.bagheri83@gmail.com>
2021-05-01 19:04:01 +0430

    Add New Post To List API

--- server/server.js
+++ server/server.js
@@ -1,5 +1,33 @@
 const express = require('express')
+const cors = require('cors')
 
 const app = express()
 
-app.listen(3001,console.log('Runnong On Port 3001...'))
+app.use(express.json());
+app.use(cors())
+
+const List = [
+    {
+        name:'Shoes',
+        time:'3/6/2021',
+        price:'10',
+        description:'These Are My Blue Shoes...'
+    }
+]
+
+app.get('/',(req,res)=>{
+    res.send(List)
+})
+
+app.post('/Create-Post',(req,res)=>{
+    const newPost = {
+        name:req.body.name,
+        time:req.body.time,
+        price:req.body.price,
+        description:req.body.description
+    }
+    List.unshift(newPost)
+    res.send('/')
+})
+
+app.listen(3001,console.log('Running On Port 3001...'))
