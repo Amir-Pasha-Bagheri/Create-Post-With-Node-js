8e34528640dc3e64e35dc248ca373175fbb714c0
Amir Pasha Bagheri <a.p.bagheri83@gmail.com>
2021-05-01 19:04:39 +0430

    Get Finall Data From Backend

--- client/src/Pages/CreatePost/CreatePost.js
+++ client/src/Pages/CreatePost/CreatePost.js
@@ -1,33 +1,53 @@
+import axios from 'axios'
 import React ,{Component} from 'react'
 import { Link } from 'react-router-dom'
 import './CreatePost.css'
+import history from '../history'
 
 export default class CreatePost extends Component {
     render(){
+
+        const SendPost = (e) =>{
+            e.preventDefault()
+            //Create Date
+            let date = new Date();
+            const dd = String(date.getDate()).padStart(2, '0');
+            const mm = String(date.getMonth() + 1).padStart(2, '0');
+            const yyyy = date.getFullYear();
+            
+            date = yyyy + '/' + mm + '/' + dd;
+
+            //Connect Server
+            axios.post('http://localhost:3001/Create-Post/',{
+                name : document.getElementById('name').value,
+                price : document.getElementById('price').value,
+                time : date,
+                description : document.getElementById('description').value
+            })
+            .then(res=>history.push(`${res.data}`))
+        }
+
         return(
             <React.Fragment>
                 CreatePost Page
                 <br/>
 
-                <form>
+                <h3 id='Message' style={{display:'none'}}>Done !</h3>
+
+                <form method='POST' onSubmit={SendPost} action='/'>
                     <div>
                         <label htmlFor='name'>Name :</label>
-                        <input type='text' name='name' placeholder='Name Of Your Product' pattern=".{3,}" required autoComplete='off' />
+                        <input id='name' type='text' name='name' placeholder='Name Of Your Product' pattern=".{3,}" required autoComplete='off' />
                     </div>
 
                     <div>
                         <label htmlFor='price'>Price :</label>
-                        <input type='number' name='price' placeholder='Your Price' required/>
-                    </div>
-
-                    <div>
-                    <label htmlFor="photo"> Choose A Picture :</label><br/>
-                        <input type="file" name="photo" accept="image/*" required/><br/>
+                        <input id='price' type='number' name='price' placeholder='Your Price' required/>
                     </div>
 
                     <div>
                         <label htmlFor="description">Description :</label><br/>
-                        <textarea rows="4" cols="30" name='description' placeholder="Write Your Description" required></textarea>
+                        <textarea id='description' rows="4" cols="30" name='description' placeholder="Write Your Description" required></textarea>
                     </div>
 
                     <br/>
--- client/src/Pages/HomePage/HomePage.js
+++ client/src/Pages/HomePage/HomePage.js
@@ -1,12 +1,28 @@
 import React ,{Component} from 'react'
 import { Link } from 'react-router-dom'
 import './HomePage.css'
+import axios from 'axios'
+import Post from '../Post'
 
 export default class HomePage extends Component {
+    state = {
+        List: []
+    }
+
+    componentDidMount(){
+        axios.get('http://localhost:3001/')
+        .then(res=>this.setState({List:res.data}))
+    }
+
     render(){
         return(
             <React.Fragment>
                 <Link to='/Create-Post'><button>Add Post</button></Link>
+
+                <br/>
+
+                {this.state.List.map((post,index)=><Post key={index} name={post.name} time={post.time} price={post.price} description={post.description}/>)}
+
             </React.Fragment>
         )
     }
